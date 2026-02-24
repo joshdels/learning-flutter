77e43f19d3dfa07ea6136ae27ec1e69a4b727ce6
joshdels <assistantgisjosh@gmail.com>
2026-02-24 09:59:43 +0800

    feat: async in dart

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,12 +1,15 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const version = '0.0.1';
+
+// run the code in the cli
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'search') {
+  } else if (arguments.first == 'wikipedia') {
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
     serachWikipedia(inputArgs);
   } else {
@@ -14,19 +17,40 @@ void main(List<String> arguments) {
   }
 }
 
-void serachWikipedia(List<String>? arguments) {
-  final String articleTitle;
+// function that handles the async
+void serachWikipedia(List<String>? arguments) async {
+  final String? articleTitle;
 
   if (arguments == null || arguments.isEmpty) {
     print('Please provide an article title.');
-    articleTitle = stdin.readLineSync() ?? '';
+    final inputFromStdin = stdin.readLineSync();
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provided. Exiting');
+      return;
+    }
+    articleTitle = inputFromStdin;
   } else {
     articleTitle = arguments.join(' ');
   }
 
-  print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
+  var articleContent = await getWikipediaArcticle(articleTitle);
+  print(articleContent);
+}
+
+// This is async call function get request
+Future<String> getWikipediaArcticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
 }
 
 void printUsage() {
